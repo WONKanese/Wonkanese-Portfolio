3ae03268048e78e6913af9af965c278172ad8763
WONKanese <21478486-WONKanese@users.noreply.replit.com>
2023-11-09 19:02:51 +0000

    Initial commit

---
+++ bgManipulate.js
@@ -0,0 +1,24 @@
+//this website was made by Harrison
+
+window.addEventListener("scroll", function() {
+  scrollBG();
+});
+function scrollBG() {
+  console.log("scroll");
+  const scrollPosition = window.scrollY;
+  const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+  const percentage = (scrollPosition / maxScroll) * 100;
+  const hue = (percentage / 100) * 360; // Map percentage to hue (0-360)
+
+  document.body.style.backgroundColor = `hsl(${hue / 2}, 50%, 50%)`;
+}
+function showDescription(title, description) {
+  if (document.getElementById("image-title").textContent == title) {
+    document.getElementById("image-title").textContent = "";
+    document.getElementById("image-description").innerHTML = "";
+    return;
+  }
+  document.getElementById("image-title").textContent = title;
+  document.getElementById("image-description").innerHTML = description;
+  document.getElementById("description-box").style.display = "block";
+}
